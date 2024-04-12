59311cbf089962e6732438a682e3968ceb7fcae1
unknown <spaudel645@gmail.com>
2024-04-12 15:59:25 -0400

    UI Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,33 +10,36 @@ import ContactForm from '@/components/home/ContactForm';
 export default function Home() {
   const [showContactForm, setShowContactForm] = useState(true)
   return (
-    <main>
-      <div className="relative p-10 flex gap-32 justify-center items-center h-screen border-b-2 border-solid border-gray-200">
-        <div onClick={() => setShowContactForm(!showContactForm)} className="p-2 group flex items-center gap-1 bg-blue-500 text-white rounded-xl cursor-pointer font-semibold hover:bg-blue-600 absolute top-10 right-28">
-        <div className='group-hover:rotate-45'><FaHandPaper /></div>Say Hello!
-        </div>
-        <div className='flex flex-2 flex-col gap-10 w-1/2'>
-          <header className="text-3xl font-bold">sudipPaudel</header>
-          <div className="text-6xl">
-            Software Engineer
+    <body style={{ overflow : showContactForm ? "hidden": "auto"}}>
+      <main  className="relative">
+        <div className="p-10 flex gap-32 justify-center items-center h-screen border-b-2 border-solid border-gray-200">
+          <div onClick={() => setShowContactForm(true)} className="p-2 group flex items-center gap-1 bg-blue-500 text-white rounded-xl cursor-pointer font-semibold hover:bg-blue-600 absolute top-10 right-28">
+          <div className='group-hover:rotate-45'><FaHandPaper /></div>Say Hello!
           </div>
-          <div className="text-2xl">
-            Full Stack Developer | React Enthusiast
-          </div>
+          <div className='flex flex-2 flex-col gap-10 w-1/2'>
+            <header className="text-3xl font-bold">sudipPaudel</header>
+            <div className="text-6xl">
+              Software Engineer
+            </div>
+            <div className="text-2xl">
+              Full Stack Developer | React Enthusiast
+            </div>
 
-          <Social />
+            <Social />
+          </div>
+          <DisplayPicture />
         </div>
-        <DisplayPicture />
-      </div>
-      <div className="relative">
-        <div className="leading-relaxed tracking-wide p-4 text-center bg-blue-200 rounded-xl w-1/2 absolute inset-x-1/4 -top-[80px]">
-          Welcome to my portfolio! I am a Toronto-based Software Engineer with 5+ years of experience. Skilled in JavaScript, React, Redux, Python, Django, and more, I specialize in crafting innovative solutions and delightful user experiences. Let's connect and explore how we can collaborate on impactful projects!<br/>
-          Projects that i have worked on
+        <div className="relative">
+          <div className="leading-relaxed tracking-wide p-4 text-center bg-blue-200 rounded-xl w-1/2 absolute inset-x-1/4 -top-[80px]">
+            Welcome to my portfolio! I am a Toronto-based Software Engineer with 5+ years of experience. Skilled in JavaScript, React, Redux, Python, Django, and more, I specialize in crafting innovative solutions and delightful user experiences. Let's connect and explore how we can collaborate on impactful projects!<br/>
+            Projects that i have worked on
+          </div>
         </div>
-      </div>
-      <Projects />
-      <Testimonials />
-      { showContactForm && <ContactForm setShowContactForm={setShowContactForm}/>}
-    </main>
+        <Projects />
+        <Testimonials />
+        { showContactForm && <ContactForm setShowContactForm={setShowContactForm}/>}
+      </main>
+
+    </body>
   )
 }
--- src/components/home/ContactForm.tsx
+++ src/components/home/ContactForm.tsx
@@ -1,29 +1,39 @@
 import { TextField } from '@mui/material';
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { VscClose } from "react-icons/vsc";
 
 type ContactFormProps = {
   setShowContactForm: (a: boolean) => void,
 }
 const ContactForm = ({ setShowContactForm }: ContactFormProps) => {
+  const nameRef = useRef();
+  const handleSubmit = () => {
+
+  }
   return (
-    <div className="z-20 absolute top-0 bottom-0 right-0 left-0 bg-white" >
+    <div className="z-20 fixed inset-0 bg-white overflow-hidden" >
+      <div className="flex p-10 border-b border-solid border-gray-300 h-32">
+        <span className="font-bold text-4xl">sudipPaudel</span>
       <div
-        className="z-30 absolute top-10 right-28 text-5xl hover:rotate-12"
+        className="z-30 absolute top-10 right-24 text-5xl hover:rotate-12"
         onClick={() => setShowContactForm(false)}
       >
         <VscClose />
       </div>
-      <div className="m-auto text-center w-1/3 mt-32">
+      </div>
+      <div className="m-auto text-center w-2/5 mt-20">
+        <div className="text-3xl leading-normal mb-12">
         Thanks for taking the time to reach out.<br />
         How can I help you today?
-        <div className="flex gap-10 w-full">
+        </div>
+        <div className="flex gap-10 w-full mb-12">
           <TextField
             id="standard-multiline-flexible"
             label="Name"
             multiline
             fullWidth
             maxRows={4}
+            ref=nameRef
             variant="standard"
           />
           <TextField
@@ -41,9 +51,10 @@ const ContactForm = ({ setShowContactForm }: ContactFormProps) => {
           label="Message"
           multiline
           rows={4}
+          className="mb-12"
           variant="standard"
         />
-
+        <div className="btn">Send</div>
       </div>
     </div>
   )
