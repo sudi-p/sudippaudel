b59a7a5c5e9e4d59632cee08620390d6b959fc0d
sudippau <38621434+sudi-p@users.noreply.github.com>
2024-04-13 14:50:47 -0400

    Contact form (#5)
    * packages updated
    
    * input
    
    * UI Complete
    
    * UI Complete

--- src/components/home/ContactForm.tsx
+++ src/components/home/ContactForm.tsx
@@ -1,29 +1,59 @@
+'use client'
 import { TextField } from '@mui/material';
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 import { VscClose } from "react-icons/vsc";
 
 type ContactFormProps = {
   setShowContactForm: (a: boolean) => void,
 }
 const ContactForm = ({ setShowContactForm }: ContactFormProps) => {
+  const form = useRef<HTMLFormElement>(null);
+  const service = process.env.EMAILJS_SERVICE_KEY;
+  console.log(service)
+  const handleSubmit = () => {
+    if (form !== null){
+      emailjs
+        .sendForm(
+          "service_4uja4ib",
+          "template_61wikvk",
+          form.current,
+          { publicKey: "Vo0TocYXdJAx4eJ22",}
+        )
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    }
+  };
   return (
-    <div className="z-20 absolute top-0 bottom-0 right-0 left-0 bg-white" >
-      <div
-        className="z-30 absolute top-10 right-28 text-5xl hover:rotate-12"
-        onClick={() => setShowContactForm(false)}
-      >
-        <VscClose />
+    <div className="z-20 fixed inset-0 bg-white overflow-hidden" >
+      <div className="flex p-10 border-b border-solid border-gray-300 h-32">
+        <span className="font-bold text-4xl">sudipPaudel</span>
+        <div
+          className="z-30 absolute top-10 right-24 text-5xl hover:rotate-12"
+          onClick={() => setShowContactForm(false)}
+        >
+          <VscClose />
+        </div>
       </div>
-      <div className="m-auto text-center w-1/3 mt-32">
-        Thanks for taking the time to reach out.<br />
-        How can I help you today?
-        <div className="flex gap-10 w-full">
+      <form ref={form} className="m-auto text-center w-2/5 mt-20">
+        <div className="text-3xl leading-normal mb-12">
+          Thanks for taking the time to reach out.<br />
+          How can I help you today?
+        </div>
+        <div className="flex gap-10 w-full mb-12">
           <TextField
             id="standard-multiline-flexible"
             label="Name"
             multiline
             fullWidth
             maxRows={4}
+            name="user_name"
             variant="standard"
           />
           <TextField
@@ -32,6 +62,7 @@ const ContactForm = ({ setShowContactForm }: ContactFormProps) => {
             multiline
             fullWidth
             maxRows={4}
+            name="user_email"
             variant="standard"
           />
         </div>
@@ -41,10 +72,12 @@ const ContactForm = ({ setShowContactForm }: ContactFormProps) => {
           label="Message"
           multiline
           rows={4}
+          name="message"
+          className="mb-12"
           variant="standard"
         />
-
-      </div>
+        <div onClick={handleSubmit} className="btn">Send</div>
+      </form>
     </div>
   )
 }
