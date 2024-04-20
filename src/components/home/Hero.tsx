477af7d14f2b867909d8d9ec5c0c3a4503d5b195
unknown <spaudel645@gmail.com>
2024-04-20 15:39:33 -0400

    display picture

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -13,15 +13,15 @@ const Hero = ({ handleClick}: HeroProps) => {
   return (
     <Section id="home">
         <div className="p-10 h-screen md:flex gap-32 justify-center items-center ">
-          <div onClick={handleClick} className="p-2 group flex items-center gap-1 bg-blue-500 text-white rounded-xl cursor-pointer font-semibold hover:bg-blue-600 absolute top-10 right-28">
+          <div onClick={handleClick} className="p-2 group flex items-center gap-1 bg-blue-500 text-white rounded-xl cursor-pointer font-semibold hover:bg-blue-600 absolute top-10 right-0 md:right-28">
             <div className='group-hover:rotate-45'><FaHandPaper /></div>Say Hello!
           </div>
-          <div className='flex flex-2 flex-col gap-10 w-1/2'>
+          <div className='flex flex-2 flex-col gap-5 md:gap-10 md:w-1/2'>
             <div className="text-3xl font-bold">Sudip Paudel</div>
-            <div className="text-6xl">
+            <div className="text-3xl md:text-6xl">
               Software Engineer
             </div>
-            <div className="text-2xl font-light">
+            <div className="text-xl md:text-2xl font-light">
               Full Stack Developer | React Enthusiast
             </div>
             <Social />
